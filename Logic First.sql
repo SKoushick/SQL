939e318aa8178f56be694a4b6b102200b9017f47
SKoushick <86823538+SKoushick@users.noreply.github.com>
2025-02-14 19:01:16 +0530

    Add files via upload

---
+++ Logic First.sql
@@ -0,0 +1,14 @@
+/*
+Multiline comment
+Character - CHAR ,VARCHAR ,TEXT (This is used when the length of the text is longer than expected), BLOB (Binary large object File = This is used to store the data of a image into binary format)
+Numeric -  INT ,DECIMAL  ,FLOAT ,DOUBLE
+
+*/
+/*
+show databases;
+drop schema LogicFirst;
+drop schema if exists LogicFirst;
+
+
+use LogicFirst;*/
+create database LogicFirst;
